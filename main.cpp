be056bdf2ff1d361f88e1f0d7212670fa0b226fb
Vladimir Kirillov <vkirillov505@gmail.com>
2025-01-15 14:22:29 +0300

    Simple fetch and insert operations added

--- main.cpp
+++ main.cpp
@@ -1,29 +1,76 @@
-#include <pqxx/pqxx>
+#include <chrono>
 #include <iostream>
+#include <pqxx/pqxx>
+
+using TimePoint = std::chrono::time_point<std::chrono::system_clock,
+                                          std::chrono::system_clock::duration>;
+
+class Timer {
+ public:
+  constexpr Timer() { m_start = std::chrono::high_resolution_clock::now(); }
+
+  ~Timer() {
+    const TimePoint end = std::chrono::high_resolution_clock::now();
+    std::cout
+        << "Time : "
+        << std::chrono::duration_cast<std::chrono::milliseconds>(end - m_start).count()
+        << std::endl;
+  }
+
+ private:
+  TimePoint m_start;
+};
+
+constexpr std::string FormConnectionString(std::string_view dbname, std::string_view user,
+                                           std::string_view password,
+                                           std::string_view host, int port) {
+  return std::format("dbname={} user={} password={} host={} port={}", dbname, user,
+                     password, host, port);
+}
+
+void FetchFromUsers(pqxx::work& work) {
+  for (const auto [name, email] : work.stream<std::string_view, std::string_view>(
+           "SELECT name, email FROM public.\"Users\"")) {
+    std::cout << name << " : " << email << std::endl;
+  }
+}
+
+void InsertIntoUsers(pqxx::work& work, std::string_view name, std::string_view email) {
+  const auto result =
+      work.exec(std::format("INSERT INTO public.\"Users\"(name, email) VALUES ({}, {});",
+                            name, email))
+          .no_rows();
+}
 
 int main() {
-    try {
-        pqxx::connection conn(
-                "dbname=postgres_db user=postgres_user password=postgres_password host=localhost port=5430");
+  try {
+    const auto connectionString = FormConnectionString(
+        "postgres_db", "postgres_user", "postgres_password", "localhost", 5430);
+    pqxx::connection conn(connectionString);
 
-        if (conn.is_open()) {
-            std::cout << "Connected to database: " << conn.dbname() << std::endl;
-        } else {
-            std::cerr << "Failed to connect to database!" << std::endl;
-            return 1;
-        }
+    pqxx::work work{conn};
 
-        pqxx::work txn(conn);
-        pqxx::result res = txn.exec("SELECT version();");
-        const auto version = res.front().front().c_str();
-        std::cout << "PostgreSQL version: " << version << std::endl;
-
-        txn.commit();
-        conn.close();
-    } catch (const std::exception &e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
+    {
+      Timer t;
+      FetchFromUsers(work);
     }
 
-    return 0;
+    {
+      Timer t;
+      InsertIntoUsers(work, "'Huyadze'", "'huyadze@mail.com'");
+    }
+
+    {
+      Timer t;
+      FetchFromUsers(work);
+    }
+
+    work.commit();
+
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+  }
+
+  return 0;
 }
